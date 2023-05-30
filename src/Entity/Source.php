462067bdb598d7edd8bd20b22ed02fe3b6f72aea
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-05-30 20:52:02 +0300

    lint: Исправления линтера

--- src/Entity/Source.php
+++ src/Entity/Source.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Source
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: "IDENTITY")]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
     #[ORM\Column]
     private readonly int $id;
 
