8570534a41d1cd7e12f7fba41aa5485fad7a8c9b
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-05-30 20:46:54 +0300

    fix: Исправлены первичные ключи у сущностей. Обновленная миграция

--- migrations/Version20230530174213.php
+++ migrations/Version20230530174213.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230528170606 extends AbstractMigration
+final class Version20230530174213 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,11 @@ final class Version20230528170606 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE rate_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE source_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE rate (id INT NOT NULL, source_id INT DEFAULT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE rate (id SERIAL NOT NULL, source_id INT DEFAULT NULL, base VARCHAR(255) NOT NULL, target VARCHAR(255) NOT NULL, rate DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_DFEC3F39953C1C61 ON rate (source_id)');
         $this->addSql('COMMENT ON COLUMN rate.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN rate.updated_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE source (id INT NOT NULL, title VARCHAR(255) NOT NULL, is_active BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE source (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, is_active BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN source.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F39953C1C61 FOREIGN KEY (source_id) REFERENCES source (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
@@ -34,8 +32,6 @@ final class Version20230528170606 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE rate_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE source_id_seq CASCADE');
         $this->addSql('ALTER TABLE rate DROP CONSTRAINT FK_DFEC3F39953C1C61');
         $this->addSql('DROP TABLE rate');
         $this->addSql('DROP TABLE source');
